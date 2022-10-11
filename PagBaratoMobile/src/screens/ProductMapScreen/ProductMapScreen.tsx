b9f50ef524e598880266b60cfd0b9e6d9293b08b
daviocasas <daviocasas@gmail.com>
2022-10-11 00:19:57 -0300

    Multiple markers done and refresh control fix

--- PagBaratoMobile/src/screens/ProductMapScreen/ProductMapScreen.tsx
+++ PagBaratoMobile/src/screens/ProductMapScreen/ProductMapScreen.tsx
@@ -6,8 +6,6 @@ import Geolocation from '@react-native-community/geolocation';
 import api from '../../services/api';
 import { getItem, StorageItems } from '../../services/storage'
 
-
-
 import * as S from './ProductMapScreen.style';
 
 
@@ -16,17 +14,27 @@ const { width, height } = Dimensions.get('screen')
 
 
 export function ProductMapScreen({ route }) {
-    const [coord, setCoord] = useState({ lat: 0, long: 0 });
+    const { params: { product, distance } } = route;
+
+    const [markerLatitude, setMarkerLatitude] = useState([]);
+    const [markerLongitude, setMarkerLongitude] = useState([]);
+    const [markerCoords, setMarkerCoords] = useState([]);
     const [currentLatitude, setCurrentLatitude] = useState(0);
     const [currentLongitude, setCurrentLongitude] = useState(0);
+    const [currentRange, setCurrentRange] = useState(Math.floor(distance));
+    const [productData, setProductData] = useState<any>(null);
+
     console.log('route', route?.params?.product);
+    console.log({ distance })
+    const prices = productData?.prices;
 
-    const { params: { product } } = route;
 
-    const fetchCoord = async () => {
+    //Retorna marcador apenas do estabelecimento mais barato
+    /*const fetchCoord = async () => {
         console.log('fetchCoord')
         const token = await getItem(StorageItems.ACCESS_TOKEN);
-        const { data } = await api.get(`/api/price/?paginate=false`, { headers: { 'Authorization': `Bearer ${token}` } })
+        const { data } = await api.get(`/api/price/?paginate=false`,
+            { headers: { 'Authorization': `Bearer ${token}` } })
         if (data) {
             const fill = data?.data.find((item) => item.productId === product.id);
 
@@ -34,14 +42,54 @@ export function ProductMapScreen({ route }) {
 
             console.log('DATA', DATA)
 
-            setCoord({ lat: DATA.data.latitude, long: DATA.data.longitude })
+            setMarkerLatitude(DATA.data.latitude)
+            setMarkerLongitude(DATA.data.longitude)
+
             console.log('DATA.LAT', DATA.data.latitude)
             console.log('DATA.LONG', DATA.data.longitude)
 
         }
+    } */
+
+    //Retorna marcador em todos os estabelecimentos que tem o produto\
+
+    const fetchCoord = async () => {
+        console.log('fetchCoord')
+        const token = await getItem(StorageItems.ACCESS_TOKEN);
+        console.log('Product Id', product.id)
+
+        try {
+            const { data: response } = await api.get(`/api/product/${product.id}?usersLatitude=${currentLatitude}&usersLongitude=${currentLongitude}&rangeRadius=${currentRange}`,
+                { headers: { 'Authorization': `Bearer ${token}` } })
+
+            console.log('DATA: ', response)
+
+            setProductData(response.data)
+            //console.log('Object: ', response.data.prices)
+            // console.log('Object (Coords): ', response.data.prices.map(x => console.log("Q ISSO: " + x.establishment)))
+            // console.log('Object (Lat): ', response.data.prices.map(x => console.log("Latitude: " + x.establishment.latitude)))
+            //console.log('Object (Long): ', response.data.prices.map(x => console.log("Longitude: " + x.establishment.longitude)))
+
+            //const markerLatitude = response.data.prices.map(x => x.establishment.latitude)
+            //const markerLongitude = response.data.prices.map(x => x.establishment.longitude)
+            //console.log("markerLatitude: ", markerLatitude)
+            //console.log("markerLongitude", markerLongitude)
+            //setMarkerLatitude(markerLatitude)
+            //setMarkerLongitude(markerLongitude)
+        } catch (erro) {
+            console.log('Erro que ta dando é esse: ')
+            console.log(erro.response)
+        }
+
+
+        //setMarkerLatitude(data.data.prices.map(x => x.establishment.latitude))
+        //setMarkerLongitude(data.data.longitude)
+
+        //console.log('DATA.LAT', DATA.data.latitude)
+        //console.log('DATA.LONG', DATA.data.longitude)
     }
 
-
+    console.log("Product Data: ", productData)
 
     const [region, setRegion] = useState(null);
 
@@ -66,13 +114,18 @@ export function ProductMapScreen({ route }) {
     }
 
     useEffect(() => {
-        fetchCoord();
-        getMyLocation()
+        getMyLocation();
     }, [])
+
+    useEffect(() => {
+        if (currentLongitude && currentLongitude) {
+            fetchCoord();
+        }
+    }, [currentLatitude, currentLongitude])
 
     return (
         <S.WrapperContainer>
-            {coord.lat && coord.long ? (
+            {productData ? (
                 <MapView
                     onMapReady={() => {
                         Platform.OS === 'android' ? PermissionsAndroid.request(
@@ -90,20 +143,26 @@ export function ProductMapScreen({ route }) {
                         longitudeDelta: 0.0421
                     }}
                     zoomEnabled={true}
-                    minZoomLevel={5}
+                    minZoomLevel={10}
                     showsUserLocation={true}
                     loadingEnabled={true}
-                //provider={PROVIDER_GOOGLE}
                 >
-                    {product && (
-                        <Marker
-                            coordinate={{ latitude: Number(coord.lat), longitude: Number(coord.lat) }}
-                            pinColor={"purple"} // any color
-                            title={product.name || ''}
-                            description={String(product.price) || ''}>
-                        </Marker>
+                    {prices && prices.length && prices.map(x => {
+                        return (
+                            <Marker
+                                key={x.id}
+                                coordinate={{
+                                    latitude: x.establishment.latitude,
+                                    longitude: x.establishment.longitude,
+                                }}
+                                pinColor={"orange"} // any color
+                                title={x.establishment.name || ''}
+                                description={String(x.value) || ''}>
+                            </Marker>
+                        )
+                    })}
 
-                    )}
+
                 </MapView>
             ) : null}
         </S.WrapperContainer>
